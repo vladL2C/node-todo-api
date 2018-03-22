3e4b3edf3b8ecc724bc1495c4fb0aa0ad354e786
Vlad <vlad@hotmail.co.nz>
2018-03-22 16:35:58 +1300

    69

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+    // db.collection('Todos').findOneAndUpdate({
+    //   _id: new ObjectID("5ab07547111f88f4acf753c5")
+    // }, {
+    //   $set: {
+    //     completed: true
+    //   }
+    // }, {
+    //   returnOriginal: false
+    // }).then((result) => {
+    //   console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+      _id: new ObjectID("5aa736047a97e5d13f3db785")
+    }, {
+      $set: {
+        name: 'Vlad'
+      },
+      $inc: {
+        age: 1
+      }
+    }, {
+      returnOriginal: false
+    }).then((result) => {
+      console.log(result);
+    });
+
+   //db.close();
+ });
