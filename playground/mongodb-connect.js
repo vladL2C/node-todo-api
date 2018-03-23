f7fe8b543d5865e152ed9af36993877132d2d3ab
Vlad <vlad@hotmail.co.nz>
2018-03-23 15:26:38 +1300

    setup mongoose ORM

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -31,5 +31,5 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 //     console.log(result.ops[0]._id.getTimestamp());
 //   });
 
-   db.close();
+   // db.close();
  });
---
+++ server/server.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo')
+// });
+
+var anotherTodo = new Todo({
+  text: "Another Todo item",
+  completed: true,
+  completedAt: 123
+});
+
+anotherTodo.save().then((doc) => {
+  console.log('Saved', doc);
+}, (e) => {
+  console.log('Unable to save todo');
+});
+
+
+
+
