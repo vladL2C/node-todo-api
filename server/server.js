b3d9679eebe0cc5cfabea02bf7d44395010ab974
vladL2C <vlad@hotmail.co.nz>
2019-01-21 12:06:21 +1300

    test ssh

--- server/server.js
+++ server/server.js
@@ -137,6 +137,7 @@ app.post('/users/login', (req, res) => {
 app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(
     () => {
+      console.log(req.token);
       res.status(200).send();
     },
     () => {
